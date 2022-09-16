5669ef76e7a7951ff19307d09a9de344f9d13fd2
anudeepragata <72305449+anudeepragata@users.noreply.github.com>
2022-09-16 18:45:19 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -33,3 +33,12 @@ def line_2_points(p, q):
 def dist_line_point(l, p):
     return abs(l.a*p.x + l.b*p.y + l.c)/(l.a**2 + l.b**2)**0.5
 
+p = Polygon([
+    linesegment(point(1,1), point(0,0)),
+    linesegment(point(1,0), point(1,1)),
+    linesegment(point(1,0), point(0,0)),
+])
+for i in p.lslist:
+    print(i.p.x, i.p.y, i.q.x, i.q.y)
+
+    
