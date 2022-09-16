5ae0fd24d2a4d1c8d57effafd80565f52afdd96a
anudeepragata <72305449+anudeepragata@users.noreply.github.com>
2022-09-16 22:18:52 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -33,6 +33,24 @@ def line_2_points(p, q):
 def dist_line_point(l, p):
     return abs(l.a*p.x + l.b*p.y + l.c)/(l.a**2 + l.b**2)**0.5
 
+def angle_lines(l, m):
+    a1, b1, a2, b2 = l.a, l.b, m.a, m.b
+    if a1*b2 + a2*b2 != 0:
+        return math.degrees(math.atan((a2*b1 - a1*b2)/(a1*b2 + a2*b2)))
+    else:
+        return math.degrees(math.pi/2)
+
+def point_on_line(p, ls):
+    l1 = line_2_points(p, ls.p)
+    l2 = line_2_points(p, ls.q)
+    l = line_2_points(ls.p, ls.q)
+
+    if angle_lines(l, l1) >= 0 and angle_lines(l, l2) >= 0:
+        return True
+    else:
+        return False
+
+
 p = Polygon([
     linesegment(point(1,1), point(0,0)),
     linesegment(point(1,0), point(1,1)),
@@ -41,4 +59,4 @@ p = Polygon([
 for i in p.lslist:
     print(i.p.x, i.p.y, i.q.x, i.q.y)
 
-    
+print(point_on_line(point(-1,1), linesegment(point(0,0), point(1,0))))
